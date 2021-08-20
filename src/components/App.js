5316eb0d07cfffea71f37d52850c6cbf8965fc54
Mehdi <semlali.mehdi.1980@gmail.com>
2021-08-20 19:06:01 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PostList() {
+    return (
+        <div>
+            Post List
+        </div>
+    )
+}
+
+export default PostList
+
