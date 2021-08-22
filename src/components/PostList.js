9160eafb940f283a0c05ad6b171b08efb37a4e38
Mehdi <semlali.mehdi.1980@gmail.com>
2021-08-22 10:18:10 +0200

    add middleware thunk

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-function PostList() {
-    return (
-        <div>
-            Post List
-        </div>
-    )
+class PostList extends Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
+    render() {
+        return (
+            <div>
+                Post List
+            </div>
+        )
+    }
 }
 
-export default PostList
 
+export default connect(null, { fetchPosts })(PostList)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+
 import App from './components/App';
+import reducers from './reducers'
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
